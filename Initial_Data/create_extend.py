1e5674e6035710afce241c946edafcc2778734bc
Ivan Ying Xuan <58112372+IvanYingX@users.noreply.github.com>
2021-01-27 14:05:16 +0100

    Added a small GUI for asking years, and leagues

---
+++ Initial_Data/create_extend.py
@@ -0,0 +1,58 @@
+import tkinter as tk
+from tkinter import messagebox
+import sys
+from . import confirm, get_years, get_leagues
+def create_or_extend():
+    root = tk.Tk()
+    
+    def on_closing():
+        if messagebox.askokcancel("Quit", "Do you want to quit?"):
+            root.destroy()
+            sys.exit('Quitting...')
+            
+    root.protocol("WM_DELETE_WINDOW", on_closing)
+    v = tk.BooleanVar()
+    w = tk.BooleanVar() 
+    tk.Label(root, 
+            text="""Do you want to Create a new Database,
+            or Extend an existing one?""",
+            justify = tk.LEFT, padx = 20).grid(row=0, column=0)
+    tk.Radiobutton(root, text="Create", indicatoron = 0, width = 30, padx = 20, variable=v, value = 1, command=root.destroy).grid(row=2, column=0)
+    tk.Radiobutton(root, text="Extend", indicatoron = 0, width = 30, padx = 20, variable=w, value = 1, command=root.destroy).grid(row=3, column=0)
+    root.mainloop()
+    return v.get()
+
+def create():
+    progress = 1
+    next_fun = 0
+    year_1 = 0
+    year_2 = 0
+    leagues = []
+    mask = []
+    while progress < 5:
+        if next_fun == -1:
+            progress -= 1
+            next_fun = 0
+        else:
+            if progress == 1:
+                leagues, mask = get_leagues.get_leagues()
+                progress += 1
+            elif progress == 2:
+                year_1 = get_years.get_initial_year()
+                if year_1 == -1:
+                    next_fun = -1
+                else:
+                    progress += 1
+            elif progress == 3:
+                year_2 = get_years.get_final_year(year_1)
+                if year_2 == -1:
+                    next_fun = -1
+                else:
+                    progress += 1
+            elif progress == 4:
+                conf = confirm.confirm(year_1, year_2, mask)
+                if conf:
+                    progress += 1
+                else:
+                    next_fun = -1
+    return year_1, year_2, leagues
---
+++ Initial_Data/get_years.py
@@ -0,0 +1,53 @@
+import tkinter as tk
+from tkinter import messagebox
+import sys
+
+def get_initial_year():
+        root = tk.Tk()
+
+        def on_closing():
+                if messagebox.askokcancel("Quit", "Do you want to quit?"):
+                        root.destroy()
+                        sys.exit('Quitting...')
+                
+        root.protocol("WM_DELETE_WINDOW", on_closing)
+        v = tk.IntVar()
+        back = tk.BooleanVar()
+        back.set(False)
+        root.title("Initial Year")
+        label = tk.Label(root, 
+                text="""Select the initial year you want to extract data from""",
+                justify = tk.LEFT, font=("Arial", 14), padx = 5, pady = 10).grid(row=0, column=1)
+        slider = tk.Scale(root, from_= 1990, to = 2020, variable=v, orient=tk.HORIZONTAL, 
+                        length=300, tickinterval=5, width=10).grid(row=1, column=1)
+        tk.Radiobutton(root, text="Confirm", indicatoron = 0, width = 10, relief=tk.FLAT, value=True, command=root.destroy).grid(row=2, column=2, pady=10)
+        tk.Radiobutton(root, text="Go Back", indicatoron = 0, width = 10, relief=tk.FLAT, variable=back, value=True, command=root.destroy).grid(row=2, column=0, pady=10)
+        root.mainloop()
+        if back.get():
+                return -1
+        return v.get()
+
+def get_final_year(year_1):
+        root = tk.Tk()
+
+        def on_closing():
+                if messagebox.askokcancel("Quit", "Do you want to quit?"):
+                        root.destroy()
+                        sys.exit('Quitting...')
+                
+        root.protocol("WM_DELETE_WINDOW", on_closing)
+
+        v = tk.IntVar()
+        back = tk.BooleanVar()
+        root.title("Final Year")
+        label = tk.Label(root, 
+                text="""Select the final year you want to extract data from""",
+                justify = tk.LEFT, font=("Arial", 14), padx = 5, pady = 10).grid(row=0, column=1)
+        slider = tk.Scale(root, from_= year_1, to = 2020, variable=v, orient=tk.HORIZONTAL, 
+                        length=300, tickinterval=5, width=10).grid(row=1, column=1)
+        tk.Radiobutton(root, text="Confirm", indicatoron = 0, width = 10, relief=tk.FLAT, value=True, command=root.destroy).grid(row=2, column=2, pady=10)
+        tk.Radiobutton(root, text="Go Back", indicatoron = 0, width = 10, relief=tk.FLAT, variable=back, value=True, command=root.destroy).grid(row=2, column=0, pady=10)
+        root.mainloop()
+        if back.get():
+                return -1
+        return v.get()
