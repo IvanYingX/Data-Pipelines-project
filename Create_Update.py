1a435ef76a756e99200db003eddb7184ff0eec3d
Ivan Ying Xuan <ying.yingxuan@hotmail.com>
2021-02-05 03:25:03 +0100

    Cleaner Repo

--- Create_Update.py
+++ Create_Update.py
@@ -1,7 +1,7 @@
-#%% 
 from Extract.Extract_Data import * 
-from Initial_Data import *
+from Initial_Gui import *
 from Create_Database import create_database
+from Update_Database import update_database
 from tkinter import messagebox
 from bs4 import BeautifulSoup
 from tkinter import filedialog
@@ -13,15 +13,18 @@ if action:
     year_1, year_2, leagues = create_update.create()
     create_database(year_1, year_2, leagues)
 else:
-    # create_update.update()
-    root = Tk()
-    root.filename = filedialog.askopenfilename(initialdir = "./",title = "Select file",filetypes = (("csv files","*.csv"),("all files","*.*")))
-    root.mainloop()
-    DIR = './' + os.path.relpath(root.filename, start = os.curdir)
-    print(pd.read_csv(DIR).head())
-    print(pd.read_csv(DIR).tail())
-    # leagues = get_leagues.get_leagues()
-    # year_1 = get_years.get_initial_year()
-    # year_2 = get_years.get_final_year(year_1)
-
-# %%
+    is_file = get_file_update.file_or_dir()
+    if is_file:
+        root = Tk()
+        gui = get_file_update.Update_Gui(root, is_file)
+        root.mainloop()
+        STA_DIR = './' + os.path.relpath(gui.path_sta, start = os.curdir)
+        RES_DIR = './' + os.path.relpath(gui.path_res, start = os.curdir)
+        update_database(RES_DIR, STA_DIR)
+    else:
+        root = Tk()
+        gui = get_file_update.Update_Gui(root, is_file)
+        root.mainloop()
+        STA_DIR = './' + os.path.relpath(gui.path_dir_sta, start = os.curdir)
+        RES_DIR = './' + os.path.relpath(gui.path_dir_res, start = os.curdir)
+        update_database(RES_DIR, STA_DIR)
