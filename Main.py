dcbc524834ec122cbfa9bb749cf47ea80d7405b4
Ivan Ying Xuan <ying.yingxuan@hotmail.com>
2021-02-28 06:39:23 +0100

    Restructured and changed the code

--- Main.py
+++ Main.py
@@ -1,34 +1,37 @@
 from Extract.Extract_Data import *
 from Initial_Gui import *
 from Create_Database import create_database
+from Create_Database import create_results_database
+from Create_Database import create_standings_database
 from Update_Database import update_database
 from tkinter import *
 import glob
 import os
 
 action = create_update.create_or_update()
-if action:
+if action == 1:
+    res_or_sta = create_update.results_or_standings()
     year_1, year_2, leagues = create_update.create()
-    create_database(year_1, year_2, leagues)
-else:
-    is_file = get_file_update.file_or_dir()
-    if is_file:
-        root = Tk()
-        gui = get_file_update.Update_Gui(root, is_file)
-        root.mainloop()
+    if res_or_sta == 1:
+        create_results_database(year_1, year_2, leagues)
+    elif res_or_sta == 2:
+        create_standings_database(year_1, year_2, leagues)
+    elif res_or_sta == 3:
+        create_database(year_1, year_2, leagues)
+
+elif action == 2:
+    y_or_l = create_update.year_or_league()
+    root = Tk()
+    gui = create_update.Update_Gui(root, y_or_l)
+    root.mainloop()
+    if y_or_l == 1:
         STA_DIR = './' + os.path.relpath(gui.path_sta, start=os.curdir)
         RES_DIR = './' + os.path.relpath(gui.path_res, start=os.curdir)
         update_database(RES_DIR, STA_DIR)
-    else:
-        root = Tk()
-        gui = get_file_update.Update_Gui(root, is_file)
-        root.mainloop()
+    elif (y_or_l == 2) or (y_or_l == 3):
         sta_dir = './' + os.path.relpath(gui.path_dir_sta, start=os.curdir)
         res_dir = './' + os.path.relpath(gui.path_dir_res, start=os.curdir)
         file_res_list = sorted(glob.glob(f'{res_dir}/*'))
         file_sta_list = sorted(glob.glob(f'{sta_dir}/*'))
         for RES_DIR, STA_DIR in list(zip(file_res_list, file_sta_list)):
             update_database(RES_DIR, STA_DIR)
-        exec(open("./Extract/Get_Team_Info.py").read())
-        os.chdir('./Extract')
-        exec(open("Get_Match_Info.py").read())
